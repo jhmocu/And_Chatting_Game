ab0796e42bd6b4eb7eb69e377532293319bc8c88
mardulce <531acdc@gamil.com>
2017-03-07 17:54:13 +0900

    이도희 ::: ProfileInfoActivity (친구 프로필) onCreate에서 setContentView 이하 확인 필요

--- app/src/main/java/edu/android/chatting_game/ProfileInfoActivity.java
+++ app/src/main/java/edu/android/chatting_game/ProfileInfoActivity.java
@@ -1,11 +1,7 @@
 package edu.android.chatting_game;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class ProfileInfoActivity
         extends AppCompatActivity {
@@ -14,18 +10,18 @@ public class ProfileInfoActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 }
--- app/src/main/java/edu/android/chatting_game/Profile_My_info.java
+++ app/src/main/java/edu/android/chatting_game/Profile_My_info.java
@@ -7,7 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Profile_My_info extends AppCompatActivity {
+public class Profile_My_info
+        extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
