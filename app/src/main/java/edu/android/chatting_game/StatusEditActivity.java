987c1720fa2c57f02da0c813fd3a0660f5a0ebea
jhmocu <jhmocu@gmail.com>
2017-03-08 14:46:26 +0900

    상대방 프로필 액티비티 전화, 내 프로필 UI 변경

---
+++ app/src/main/java/edu/android/chatting_game/StatusEditActivity.java
@@ -0,0 +1,14 @@
+package edu.android.chatting_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatusEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_edit);
+
+    }
+}
