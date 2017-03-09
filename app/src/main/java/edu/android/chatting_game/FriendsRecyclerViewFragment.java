6b97d7f63055c692026506295ecb5a1ebbae8cbd
LeeASim <gkcmzl12@gmail.com>
2017-03-09 17:06:41 +0900

    저장기능 완성

--- app/src/main/java/edu/android/chatting_game/FriendsRecyclerViewFragment.java
+++ app/src/main/java/edu/android/chatting_game/FriendsRecyclerViewFragment.java
@@ -111,7 +111,7 @@ public class FriendsRecyclerViewFragment
     }
 
     private void startProfileActivity(int position) {
-        if (position == 0){
+        if (position == 0) {
             Intent intent = new Intent(getContext(), Profile_My_info.class);
             intent.putExtra(KEY_EXTRA_IMAGEID, list.get(position).getImageId());
             intent.putExtra(KEY_EXTRA_NAME, list.get(position).getName());
@@ -126,5 +126,5 @@ public class FriendsRecyclerViewFragment
             intent.putExtra(KEY_EXTRA_MESSAGE, list.get(position).getMessage());
             startActivity(intent);
         }
-
+    }
 }
