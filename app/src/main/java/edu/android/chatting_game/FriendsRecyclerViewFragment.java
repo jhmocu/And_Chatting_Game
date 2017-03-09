5c8ec384c6a25be744782a68d249327dd81df474
jhmocu <jhmocu@gmail.com>
2017-03-09 15:30:30 +0900

    목진혁:프사 친구 프사 구분

--- app/src/main/java/edu/android/chatting_game/FriendsRecyclerViewFragment.java
+++ app/src/main/java/edu/android/chatting_game/FriendsRecyclerViewFragment.java
@@ -111,12 +111,22 @@ public class FriendsRecyclerViewFragment
     }
 
     private void startProfileActivity(int position) {
-        Intent intent = new Intent(getContext(), ProfileInfoActivity.class);
-        intent.putExtra(KEY_EXTRA_IMAGEID, list.get(position).getImageId());
-        intent.putExtra(KEY_EXTRA_NAME, list.get(position).getName());
-        intent.putExtra(KEY_EXTRA_PHONENUMBER, list.get(position).getPhoneNumber());
-        intent.putExtra(KEY_EXTRA_MESSAGE, list.get(position).getMessage());
-        startActivity(intent);
+        if (position == 0){
+            Intent intent = new Intent(getContext(), Profile_My_info.class);
+            intent.putExtra(KEY_EXTRA_IMAGEID, list.get(position).getImageId());
+            intent.putExtra(KEY_EXTRA_NAME, list.get(position).getName());
+            intent.putExtra(KEY_EXTRA_PHONENUMBER, list.get(position).getPhoneNumber());
+            intent.putExtra(KEY_EXTRA_MESSAGE, list.get(position).getMessage());
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(getContext(), ProfileInfoActivity.class);
+            intent.putExtra(KEY_EXTRA_IMAGEID, list.get(position).getImageId());
+            intent.putExtra(KEY_EXTRA_NAME, list.get(position).getName());
+            intent.putExtra(KEY_EXTRA_PHONENUMBER, list.get(position).getPhoneNumber());
+            intent.putExtra(KEY_EXTRA_MESSAGE, list.get(position).getMessage());
+            startActivity(intent);
+        }
+
     }
 
 }
