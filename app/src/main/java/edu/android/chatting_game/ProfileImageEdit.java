9b4c083db15c2c201c31aae7207dc634ee841b7f
HajinKim <audcjf1003@naver.com>
2017-03-09 10:57:31 +0900

    프로필 관련1

--- app/src/main/java/edu/android/chatting_game/ProfileImageEdit.java
+++ app/src/main/java/edu/android/chatting_game/ProfileImageEdit.java
@@ -1,11 +1,19 @@
 package edu.android.chatting_game;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 
 /**
@@ -13,6 +21,11 @@ import android.view.ViewGroup;
  */
 public class ProfileImageEdit extends Fragment {
 
+    private static final int PICK_FROM_ALBUM = 100;
+
+    private File photoFile;
+    private ImageView imageView;
+    private Button button;
 
     public ProfileImageEdit() {
         // Required empty public constructor
@@ -23,7 +36,32 @@ public class ProfileImageEdit extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_image_edit, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_image_edit, container, false);
+        imageView = (ImageView) view.findViewById(R.id.imageView);
+        button = (Button) view.findViewById(R.id.btnEdit);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                albumSelect();
+
+            }
+        });
+        return view;
+    }
+    private void albumSelect() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data != null) {
+            Uri uri = data.getData();
+            Log.i("이미지 경로", uri.toString());
+            imageView.setImageURI(uri);
+        }
+    }
 }
+
