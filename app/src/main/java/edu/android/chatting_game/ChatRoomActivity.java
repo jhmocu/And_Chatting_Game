879461ce00199e3ee02cb40abe40e30999ec270e
mardulce <531acdc@gamil.com>
2017-03-10 14:20:10 +0900

    이도희 ::: ChatRoomActivity 상단 메뉴 수정

--- app/src/main/java/edu/android/chatting_game/ChatRoomActivity.java
+++ app/src/main/java/edu/android/chatting_game/ChatRoomActivity.java
@@ -1,10 +1,13 @@
 package edu.android.chatting_game;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 public class ChatRoomActivity
@@ -13,13 +16,28 @@ public class ChatRoomActivity
     private String title;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_chat_room, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.search: break;
+            case R.id.chat_room_option1: break;
+            case R.id.chat_room_option2: break;
+        } //end switch
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
-
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        // TODO: 2017-03-09 title - 대화 상대로 set
-        collapsingToolbarLayout.setTitle("뭐왜뭐");
 
         MessageListFragment messageListFragment = new MessageListFragment();
         SendMessageFragment sendMessageFragment = new SendMessageFragment();
@@ -29,9 +47,11 @@ public class ChatRoomActivity
         transaction.add(R.id.messageListFrame, messageListFragment);
         transaction.commit();
 
-
-
-
+        ActionBar actionBar = getSupportActionBar();
+        // TODO: 2017-03-10 title: 대화상대로 set 하는 public 메소드 만들기
+//        actionBar.hide();
+        title = "대화방 이름";
+        actionBar.setTitle(title);
 
     }
 }
