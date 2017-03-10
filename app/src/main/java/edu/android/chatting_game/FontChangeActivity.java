60a2eab28a5aac45168d5b541cb1a089ddbe606d
HajinKim <audcjf1003@naver.com>
2017-03-10 10:57:51 +0900

    설정창의 UI와 각 버튼에 대한 xml, Activity파일, 각 버튼의 Intent rngus

---
+++ app/src/main/java/edu/android/chatting_game/FontChangeActivity.java
@@ -0,0 +1,13 @@
+package edu.android.chatting_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FontChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_font_change);
+    }
+}
