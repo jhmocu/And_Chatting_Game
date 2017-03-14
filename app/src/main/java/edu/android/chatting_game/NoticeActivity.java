3b5ae2012cd649e64a98b014eee9ac98f30794fa
heajin <gpwlsdl153@gmail.com>
2017-03-14 15:37:27 +0900

    공지사항 중간업데이트

--- app/src/main/java/edu/android/chatting_game/NoticeActivity.java
+++ app/src/main/java/edu/android/chatting_game/NoticeActivity.java
@@ -1,13 +1,20 @@
 package edu.android.chatting_game;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class NoticeActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
+
+        listView = (ListView) findViewById(R.id.noticeListView);
+
+
     }
 }
