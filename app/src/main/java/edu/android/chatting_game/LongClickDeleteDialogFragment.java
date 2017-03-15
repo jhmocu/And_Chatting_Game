22df298cbac56698d21be400f70b0d9282c9c0f6
heajin <gpwlsdl153@gmail.com>
2017-03-15 17:58:49 +0900

    친구삭제기능 구현중

--- app/src/main/java/edu/android/chatting_game/LongClickDeleteDialogFragment.java
+++ app/src/main/java/edu/android/chatting_game/LongClickDeleteDialogFragment.java
@@ -1,24 +1,60 @@
 package edu.android.chatting_game;
 
+
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LongClickDeleteDialogFragment extends DialogFragment {
 
+    private RecyclerView recyclerView;
+
+    private FriendDeleteCallback callback;
 
     public LongClickDeleteDialogFragment() {
         // Required empty public constructor
     }
 
+    class LongClickDeleteViewHolder extends RecyclerView.ViewHolder{
+
+        public LongClickDeleteViewHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    callback.deleteFriend(position);
+
+                }
+            });
+        }
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof FriendDeleteCallback) {
+            callback = (FriendDeleteCallback) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
 
     @NonNull
     @Override
@@ -41,5 +77,10 @@ public class LongClickDeleteDialogFragment extends DialogFragment {
         });
 
         return builder.create();
+    }
+
+
+    public interface FriendDeleteCallback {
+        void deleteFriend(int position);
     }
 }
