ead71ce4f55267ca6a32f0be43d4ea625d3c8374
LeeASim <gkcmzl12@gmail.com>
2017-03-16 10:32:46 +0900

    꾸밈-색상, 채팅창 버튼 두개

--- app/src/main/java/edu/android/chatting_game/Long_Click_name_Update.java
+++ app/src/main/java/edu/android/chatting_game/Long_Click_name_Update.java
@@ -11,8 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -30,7 +30,7 @@ import java.io.InputStreamReader;
 public class Long_Click_name_Update extends AppCompatActivity {
 
     private EditText textView;
-    private Button btn;
+    private ImageButton btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +43,7 @@ public class Long_Click_name_Update extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         textView = (EditText) findViewById(R.id.editName);
-        btn = (Button) findViewById(R.id.button);
+        btn = (ImageButton) findViewById(R.id.button);
         final Bundle extra = getIntent().getExtras();
         if (extra != null) {
             String name = extra.getString(FriendsRecyclerViewFragment.KEY_EXTRA_NAME2);
