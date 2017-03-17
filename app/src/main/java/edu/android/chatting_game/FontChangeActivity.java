b813f78f7b0879342e624025d3cb2d23f9fcf26f
LeeASim <gkcmzl12@gmail.com>
2017-03-17 16:32:10 +0900

    fontChange-나중에 체팅방 완성시 작업

--- app/src/main/java/edu/android/chatting_game/FontChangeActivity.java
+++ app/src/main/java/edu/android/chatting_game/FontChangeActivity.java
@@ -2,6 +2,8 @@ package edu.android.chatting_game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -10,39 +12,48 @@ public class FontChangeActivity extends Activity{
 
     private SeekBar seekBar;
     private TextView text;
+    private Button selectBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_font_change);
 
-//        int size=10;
-//        seekBar=(SeekBar)findViewById(R.id.textsize);
-//        text=(TextView) findViewById(R.id.textMyMsg);
-//
-//
-//        seekBar.setMax(30);
-//        seekBar.setProgress(size);
-//        text.setTextSize((float)10);
-//
-//
-//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
-//                text.setTextSize(progress);
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
-//
+        int size=15;
+        seekBar=(SeekBar)findViewById(R.id.textsize);
+        text=(TextView)findViewById(R.id.textTest);
+        selectBtn=(Button)findViewById(R.id.SelectBtn);
+
+
+        seekBar.setMax(30);
+        seekBar.setProgress(size);
+        text.setTextSize((float)15);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+            text.setTextSize(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: 나중에 ChatRoomActivity 에서 my_Message, your_Message 완성후 작업
+            }
+        });
 
     }
 }
