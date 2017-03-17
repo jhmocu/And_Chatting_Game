fde214be55b4edcc15a81cd26d5fcc0bd2a9bae9
LeeASim <gkcmzl12@gmail.com>
2017-03-17 18:38:01 +0900

    프로필 내가위로

--- app/src/main/java/edu/android/chatting_game/FriendsRecyclerViewFragment.java
+++ app/src/main/java/edu/android/chatting_game/FriendsRecyclerViewFragment.java
@@ -17,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 import it.sephiroth.android.library.picasso.Picasso;
 
 /**
@@ -67,6 +69,9 @@ public class FriendsRecyclerViewFragment
     class FriendAdapter
             extends RecyclerView.Adapter<FriendViewHolder> {
 
+
+
+
         @Override
         public FriendViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
@@ -78,6 +83,7 @@ public class FriendsRecyclerViewFragment
         @Override
         public void onBindViewHolder(final FriendViewHolder holder, int position) {
             Friend friend = list.get(position);
+
             Picasso.with(holder.itemView.getContext()).load(Uri.parse(friend.getPic_url())).resize(100, 100).centerCrop().into(holder.photo);
             holder.name.setText(friend.getfName());
             holder.message.setText(friend.getStatus_msg());
@@ -112,17 +118,16 @@ public class FriendsRecyclerViewFragment
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_container);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new FriendAdapter());
+        Collections.reverse(list);
         return view;
     }
 
     private void startProfileActivity(int position) {
-        if (position==list.size()-1) {
+        if (position==0) {
             Intent intent = new Intent(getContext(), Profile_My_info.class);
             intent.putExtra(KEY_EXTRA_IMAGEURL, list.get(position).getPic_url());
             intent.putExtra(KEY_EXTRA_NAME, list.get(position).getfName());
             intent.putExtra(KEY_EXTRA_MESSAGE, list.get(position).getStatus_msg());
-
-
             startActivity(intent);
 //            intent.putExtra(KEY_EXTRA_PHONENUMBER, list.get(position).getPhoneNumber());
         } else {
