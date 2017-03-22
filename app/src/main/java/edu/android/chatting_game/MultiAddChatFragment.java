f2c1f2032363b012b4783974b6e1e6bef0cd1d7c
HajinKim <audcjf1003@naver.com>
2017-03-22 14:51:40 +0900

    다시

--- app/src/main/java/edu/android/chatting_game/MultiAddChatFragment.java
+++ app/src/main/java/edu/android/chatting_game/MultiAddChatFragment.java
@@ -19,6 +19,7 @@ public class MultiAddChatFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
